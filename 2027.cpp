4c4f5cfa3c99c24a649c7f298e3f13a6ccbe3b0d
Linsen-lsjs <1242654668@qq.com>
2026-02-01 18:18:00 +0800

    题号：2027

---
+++ 2027.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=0,i=0,j=0;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+          cout<<"*";  
+        }
+    cout<<endl;
+    }
+    return 0;
+}
